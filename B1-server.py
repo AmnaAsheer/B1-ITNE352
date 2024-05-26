c9b7b5b4bf6aee75728a6df23dd539360e574a04
amna hesham <amnahesham12@email.com>
2024-05-26 17:48:43 +0300

    countries list

--- B1-server.py
+++ B1-server.py
@@ -2,3 +2,14 @@ import socket
 import json
 import os
 import threading
+countries = {
+    "Australia": "au",
+    "New Zealand": "nz",
+    "Canada": "ca",
+    "United Arab Emirates": "ae",
+    "Saudi Arabia": "sa",
+    "United Kingdom": "gb",
+    "United States": "us",
+    "Egypt": "eg",
+    "Morocco": "ma",
+}
