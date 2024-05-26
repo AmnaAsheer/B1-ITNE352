772ecea8c693b4176100291bb1fd86c03bcf8f26
amna hesham <amnahesham12@email.com>
2024-05-26 18:42:31 +0300

    creat mainrequest function

--- B1-client.py
+++ B1-client.py
@@ -1,2 +1,9 @@
 import socket
 import json
+def mainrequest(SocketC, request):
+    SocketC.send(request.encode('utf-8'))
+    response_length_str = SocketC.recv(10).decode('utf-8').strip()
+    print(f"Received response length string: '{response_length_str}'")
+    response_length = int(response_length_str)
+    print(f"Received response length: {response_length}")
+    response_data = b""
