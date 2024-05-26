a6a7b2424d2a6056c88972797b2d58c8beb4e3cf
amna hesham <amnahesham12@email.com>
2024-05-26 18:49:52 +0300

    creat a menu for the clinet viwe

--- B1-client.py
+++ B1-client.py
@@ -12,3 +12,20 @@ def mainrequest(SocketC, request):
         response_data += part
 
     return json.loads(response_data.decode('utf-8'))
+
+def search_headlines_menu(SocketC):
+    while True:
+        print("Search headlines menu:")
+        print("1. Search for keywords")
+        print("2. Search by category")
+        print("3. Search by country")
+        print("4. List all new headlines")
+        print("5. Back to the main menu")
+        option = input("Select an option: ")
+
+        if option == '1':
+            keyword = input("Enter keyword: ")
+            params = {'q': keyword}
+            news_data = mainrequest(SocketC, f'get_news|everything|{json.dumps(params)}')
+            display_items(news_data)
+        elif option == '2':
